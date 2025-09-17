44fc65fffe5c190426f3ba5c837a7a03408e6677
wiktor-falek <falekwiktor@gmail.com>
2025-09-17 23:26:57 +0200

    print error, initialize stack, formatting

--- src/arch/x86_64/boot.asm
+++ src/arch/x86_64/boot.asm
@@ -1,9 +1,27 @@
 global start
 
+
 section .text
 bits 32
+
 start:
-    ; print  to screen
-    mov dword [0xb8000], 0x2f4b2f4f
+    mov esp, stack_top ; initialize stack
+
     hlt
 
+error:
+    ; 0xb8000 = VGA text buffer, 4000 bytes long, each cell is 2 bytes (1 ASCII 1 color attribute)
+    ; print "ERROR: {code}"
+    mov dword [0xb8000], 0x0f520f45
+    mov dword [0xb8004], 0x0f4f0f52
+    mov dword [0xb8008], 0x0f3a0f52
+    mov word [0xb80012], 0x0f20
+    mov byte [0xb80014], al
+    hlt
+
+
+section .bss
+
+stack_bottom:
+    resb 64
+stack_top:
