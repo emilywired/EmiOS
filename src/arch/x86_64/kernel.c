a1bc507d337ca6a3a2c34246d16d3604df4b0e5b
wiktor-falek <falekwiktor@gmail.com>
2025-09-22 00:52:49 +0200

    write to vga in c

---
+++ src/arch/x86_64/kernel.c
@@ -0,0 +1,34 @@
+#include <stdint.h>
+
+void print_welcome_message() {
+    volatile uint16_t* vga = (uint16_t*)0xb8000;
+
+    vga[0] = 0xdf57;
+    vga[1] = 0xdf65;
+    vga[2] = 0xdf6c;
+    vga[3] = 0xdf63;
+    vga[4] = 0xdf6f;
+    vga[5] = 0xdf6d;
+    vga[6] = 0xdf65;
+
+    vga[7] = 0xdf20;
+
+    vga[8] = 0xdf74;
+    vga[9] = 0xdf6f;
+
+    vga[10] = 0xdf20;
+
+    vga[11] = 0xdb45;
+    vga[12] = 0xdb6d;
+    vga[13] = 0xdb69;
+    vga[14] = 0xdb4f;
+    vga[15] = 0xdb53;
+
+    vga[16] = 0xdf21;
+    vga[17] = 0xdf21;
+    vga[18] = 0xdf21;
+}
+
+void kmain(void) {
+    print_welcome_message();
+}
--- src/arch/x86_64/long_mode_init.asm
+++ src/arch/x86_64/long_mode_init.asm
@@ -18,31 +18,8 @@ long_mode_start:
     mov rcx, 0
     call .clear_vga_buffer
 
-    ; print "Welcome to EmiOS!!!"
-    mov word [0xb8000], 0xdf57
-    mov word [0xb8002], 0xdf65
-    mov word [0xb8004], 0xdf6c
-    mov word [0xb8006], 0xdf63
-    mov word [0xb8008], 0xdf6f
-    mov word [0xb800A], 0xdf6d
-    mov word [0xb800C], 0xdf65
-
-    mov word [0xb800E], 0xdf20
-
-    mov word [0xb8010], 0xdf74
-    mov word [0xb8012], 0xdf6f
-
-    mov word [0xb8014], 0xdf20
-
-    mov word [0xb8016], 0xdb45
-    mov word [0xb8018], 0xdb6d
-    mov word [0xb801A], 0xdb69
-    mov word [0xb801C], 0xdb4f
-    mov word [0xb801E], 0xdb53
-
-    mov word [0xb8020], 0xdf21
-    mov word [0xb8022], 0xdf21
-    mov word [0xb8024], 0xdf21
+    extern kmain
+    call kmain
 
     hlt
 
